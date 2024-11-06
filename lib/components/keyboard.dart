e7e40f7d5f396125d55e88fd28e08761ca0279ce
Vitor Moura <jvrezendemoura@gmail.com>
2024-11-06 17:41:13 -0300

    finalizing the calculator memory

--- lib/components/keyboard.dart
+++ lib/components/keyboard.dart
@@ -3,6 +3,10 @@ import 'package:calculadora/components/button_row.dart';
 import 'package:flutter/material.dart';
 
 class Keyboard extends StatelessWidget {
+  final void Function(String) cb;
+
+  Keyboard(this.cb);
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -10,24 +14,33 @@ class Keyboard extends StatelessWidget {
       child: Column(
         children: <Widget>[
           ButtonRow([
-            Button.extras("C"),
-            Button.extras("+/-"),
-            Button.extras("%"),
-            Button.operation("/")
+            Button.extras("AC", cb: cb),
+            Button.extras("+/-", cb: cb),
+            Button.extras("%", cb: cb),
+            Button.operation("/", cb: cb)
           ]),
-          ButtonRow(
-              [Button("1"), Button("2"), Button("3"), Button.operation("x")]),
-          ButtonRow(
-              [Button("4"), Button("5"), Button("7"), Button.operation("-")]),
-          ButtonRow(
-              [Button("7"), Button("8"), Button("9"), Button.operation("+")]),
           ButtonRow([
-            Button(
-              "0",
-              flex_value: 2,
-            ),
-            Button("."),
-            Button.operation("=")
+            Button("7", cb: cb),
+            Button("8", cb: cb),
+            Button("9", cb: cb),
+            Button.operation("x", cb: cb)
+          ]),
+          ButtonRow([
+            Button("4", cb: cb),
+            Button("5", cb: cb),
+            Button("6", cb: cb),
+            Button.operation("-", cb: cb)
+          ]),
+          ButtonRow([
+            Button("1", cb: cb),
+            Button("2", cb: cb),
+            Button("3", cb: cb),
+            Button.operation("+", cb: cb)
+          ]),
+          ButtonRow([
+            Button("0", flex_value: 2, cb: cb),
+            Button(".", cb: cb),
+            Button.operation("=", cb: cb)
           ]),
         ],
       ),
--- lib/screens/calculadora.dart
+++ lib/screens/calculadora.dart
@@ -1,8 +1,22 @@
 import "package:calculadora/components/display.dart";
+import "package:calculadora/models/memory.dart";
 import "package:flutter/material.dart";
 import "package:calculadora/components/keyboard.dart";
 
-class Calculadora extends StatelessWidget {
+class Calculadora extends StatefulWidget {
+  @override
+  _CalculadoraState createState() => _CalculadoraState();
+}
+
+class _CalculadoraState extends State<Calculadora> {
+  final Memory memory = Memory();
+
+  _onPressed(String command) {
+    setState(() {
+      memory.applyCommand(command);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -11,7 +25,7 @@ class Calculadora extends StatelessWidget {
             useMaterial3: true,
             colorScheme: ColorScheme.fromSeed(seedColor: Colors.black)),
         home: Column(
-          children: <Widget>[Display("123.45"), Keyboard()],
+          children: <Widget>[Display(memory.value), Keyboard(_onPressed)],
         ));
   }
 }
